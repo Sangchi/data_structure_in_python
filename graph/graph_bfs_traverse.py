de19ab0dbd77ed4d17dec388f2825e70809be287
Sangchi <prashnatchavan726386@gmail.com>
2024-08-16 18:11:17 +0530

    []

---
+++ graph/graph_bfs_traverse.py
@@ -0,0 +1,37 @@
+class Graph:
+
+    def __init__(self,gdict=None):
+        if gdict is None:
+            gdict={}
+
+        else:
+            self.gdict=gdict
+
+    def bfs_traverse(self,vertex):
+
+        visited=set()
+        visited.add(vertex)
+        queue=[vertex]
+
+        while queue:
+            current_vertex=queue.pop(0)
+            print(current_vertex)
+
+            for node in self.gdict[current_vertex]:
+                if node not in visited:
+                    visited.add(node)
+                    queue.append(node)
+
+
+custom_dict={
+
+    "A":["B","C"],
+    "B":["A","E"],
+    "C":["A","D"],
+    "D":["C","E"],
+    "E":["B","D"]
+}
+
+graph=Graph(custom_dict)
+graph.bfs_traverse("B")
+            
